eb5771e8f6dc4a9186c09ddae8482a8f392a88c4
Bhanu-0711 <bhanu7pratap11@gmail.com>
2025-09-01 20:20:37 +0530

    Traversal

--- code.cpp
+++ code.cpp
@@ -20,12 +20,6 @@ int main(){
     cout<<"capacity of vector 1"<<v.capacity()<<endl;
 
     for (int i=0;i<v.size();i++)
-        cout<<v.at(i)<<" ";
-    cout<<endl;
-    for(auto value:v)
-        cout<<value<<" ";
-    cout<<endl;
-    v.pop_back();
     for(auto value:v)
         cout<<value<<" ";
     cout<<endl;
